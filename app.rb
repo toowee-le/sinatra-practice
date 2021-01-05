d176ad9cb14c11b3c2405975715f2da3b601481d
Tuyet Le <toowee.le@gmail.com>
2021-01-05 20:29:41 +0000

    Add a views folder to separate the Ruby and HTML code

--- app.rb
+++ app.rb
@@ -3,15 +3,13 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  "Hello World!"
+  erb(:index)
 end
 
 get '/secret' do
-  "My page of secrets..."
+  erb(:index)
 end
 
 get '/cat' do
-  '<div>
-    <img style="border:10px solid #000;" src="http://www.oprah.com/g/image-resizer?width=670&link=http://static.oprah.com/images/o2/201605/201605-orig-two-cats-949x534.jpg">
-  </div>'
+  erb(:index)
 end
