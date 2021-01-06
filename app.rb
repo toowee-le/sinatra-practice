8501913de5603dd377f52af3ca6ebd523af434b0
Tuyet Le <toowee.le@gmail.com>
2021-01-06 13:58:43 +0000

    Build new routes to use params

--- app.rb
+++ app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   "Shhh... it's a secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
